a5c1f8341b6989ce372c6e74eb3229e25b231e06
sassi.walid@outlook.com <sassi.walid@outlook.com>
2019-02-10 13:38:46 +0100

    Initial Commit

---
+++ versi-app-devlops/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  versi-app-devlops
+//
+//  Created by MacBook Pro on 10/02/2019.
+//  Copyright © 2019 Walid Sassi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
